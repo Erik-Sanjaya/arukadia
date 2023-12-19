453695b253dcab03cefc539db92594815fe80906
Erik Sanjaya <erik.sanjaya25@gmail.com>
2023-12-20 01:56:11 +0700

    vga text mode

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
+use core::fmt::Write;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    print!("Hello world{}", "!");
+
+    panic!("FUCKh");
+
     loop {}
 }
